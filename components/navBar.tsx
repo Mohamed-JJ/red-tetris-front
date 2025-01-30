e9f734753cba65f3514bab84325a8a3b001675b1
med <simojarboue24@gmail.com>
2025-01-30 23:50:42 +0100

    added the routing to the navBar

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -10,9 +10,11 @@ import { CgGames } from 'react-icons/cg';
 import { MdOutlineScoreboard } from 'react-icons/md';
 import { CgMinimize } from 'react-icons/cg';
 import MinimizedNavBar from './SignIn/minimizedNavBar';
+import { useRouter } from 'next/navigation';
 
 const NavBar = () => {
   const [isMinimized, setIsMinimized] = useState<boolean>(false);
+  const router = useRouter();
 
   const toggleIsMinimized = () => {
     setIsMinimized(!isMinimized);
@@ -32,24 +34,36 @@ const NavBar = () => {
     <main className="w-[450px] h-[80px] bg-[#AAADFA] absolute bottom-10 right-1/2 translate-x-1/2 rounded-[50px]">
       <div className="w-full h-full relative flex justify-around gap-10">
         <div className=" flex items-center gap-10">
-          <div className="size-[50px] rounded-full flex items-center justify-center bg-main ">
+          <div
+            className="size-[50px] rounded-full flex items-center justify-center bg-main "
+            onClick={() => router.push('/dashboard/Profile')}
+          >
             <AiOutlineUser className="size-[25px] text-[#AAADFA] hover:cursor-pointer hover:scale-125 duration-100" />
           </div>{' '}
-          <div className="size-[50px] rounded-full flex items-center justify-center bg-main ">
+          <div
+            className="size-[50px] rounded-full flex items-center justify-center bg-main "
+            onClick={() => router.push('/dashboard/Settings')}
+          >
             <BsGear className="size-[25px] text-[#AAADFA] hover:cursor-pointer hover:scale-125 duration-100" />
           </div>
         </div>
         <div className=" flex items-center gap-10">
-          <div className="size-[50px] rounded-full flex items-center justify-center bg-main ">
+          <div
+            className="size-[50px] rounded-full flex items-center justify-center bg-main "
+            onClick={() => router.push('/dashboard/Game')}
+          >
             <CgGames className="size-[25px] text-[#AAADFA] hover:cursor-pointer hover:scale-125 duration-100" />
           </div>
-          <div className="size-[50px] rounded-full flex items-center justify-center bg-main ">
+          <div
+            className="size-[50px] rounded-full flex items-center justify-center bg-main "
+            onClick={() => router.push('/dashboard/LeaderBoard')}
+          >
             <MdOutlineScoreboard className="size-[25px] text-[#AAADFA] hover:cursor-pointer hover:scale-125 duration-100" />
           </div>
         </div>
         <div className="size-[100px] -top-9 bg-[#AAADFA] absolute flex items-center justify-center right-1/2 translate-x-1/2 rounded-full">
           <div className="size-[65px] bg-main rounded-full  flex items-center justify-center">
-            <div className="size-24  hover:cursor-pointer hover:scale-105 duration-100">
+            <div className="size-24  hover:cursor-pointer hover:scale-105 duration-100" onClick={() => router.push('/dashboard')}>
               <Canvas>
                 <Environment preset="city" />
                 <OrbitControls enableZoom={false} />
