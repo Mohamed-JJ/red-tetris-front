2c09906d8a5b28170b9aba80e05918ddbc335e77
med <simojarboue24@gmail.com>
2025-01-31 13:32:11 +0100

    added the skeleton to the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
 
 const page = () => {
-  return <div>Dashboard</div>;
+  return (
+    <main className="w-full h-full scrollable overflow-x-hidden">
+      <div className="grid grid-cols-2 gap-x-44 gap-y-10 px-32 py-20">
+        <div className="bg-blue-500 h-[500px] flex items-center justify-center">
+          1
+        </div>
+        <div className="bg-green-500 h-[500px] flex items-center justify-center">
+          2
+        </div>
+        <div className="bg-red-500 h-[500px] flex items-center justify-center">
+          3
+        </div>
+        <div className="bg-yellow-500 h-[500px] flex items-center justify-center">
+          4
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default page;
