39cae9017ab344a95c610533b8ec4b2ccbe7df5a
mohamed jarboua <mjarboua@mohameds-MacBook-Pro.local>
2025-06-30 15:42:24 +0100

    changed auth page style

--- components/forms.tsx
+++ components/forms.tsx
@@ -1,21 +1,24 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { FaEye } from 'react-icons/fa';
-import { FaEyeSlash } from 'react-icons/fa6';
-import { FcGoogle } from 'react-icons/fc';
-import { Si42 } from 'react-icons/si';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { LoginInput, SignUpInput } from '@/lib/types';
+import React, { useState } from "react";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
+import { Si42 } from "react-icons/si";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { LoginInput, SignUpInput } from "@/lib/types";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { RootState } from '@/app/state/store';
-import { setUser } from '@/app/state/user/userSlice';
-import { useRouter } from 'next/navigation';
-import { api } from '@/utils';
-import { setToken } from '@/utils';
-import { toast } from 'react-toastify';
+import { RootState } from "@/app/state/store";
+import { setUser } from "@/app/state/user/userSlice";
+import { useRouter } from "next/navigation";
+import { api } from "@/utils";
+import { setToken } from "@/utils";
+import { toast } from "react-toastify";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 export const SignUpForm = ({ toggle }: { toggle: (arg: boolean) => void }) => {
   const { register, handleSubmit, reset } = useForm<SignUpInput>();
@@ -25,7 +28,7 @@ export const SignUpForm = ({ toggle }: { toggle: (arg: boolean) => void }) => {
     try {
       window.location.href = link;
     } catch (error: unknown) {
-      console.log('error in google auth:', error);
+      console.log("error in google auth:", error);
     }
   };
   const ToggleShowPassWord = () => {
@@ -34,16 +37,16 @@ export const SignUpForm = ({ toggle }: { toggle: (arg: boolean) => void }) => {
   const onSubmit: SubmitHandler<SignUpInput> = async (data) => {
     try {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      console.log('user object', data);
-      const createdUser = await api.post('/user', data);
+      console.log("user object", data);
+      const createdUser = await api.post("/user", data);
       console.log(createdUser);
-      notify('please go to the login page to login');
+      notify("please go to the login page to login");
 
       reset();
       toggle(true);
       // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
     } catch (error: any) {
-      notify('error in creating an account');
+      notify("error in creating an account");
     }
   };
   return (
@@ -51,40 +54,40 @@ export const SignUpForm = ({ toggle }: { toggle: (arg: boolean) => void }) => {
       <form
         action="submit"
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-3"
+        className="flex flex-col gap-4 w-[60%]"
       >
         <div className="w-full flex gap-5">
-          <input
+          <Input
             type="text"
-            className="pl-3 bg-authInputBg text-white w-[199px] h-[62px] border-2 border-authInputBorder rounded-[14px] placeholder:text-authPlaceHolderColor text-[18px] font-bold"
+            className="placeholder:text-white text-white h-[50px]"
             placeholder="first name"
-            {...register('firstName', { required: true })}
+            {...register("firstName", { required: true })}
           />
-          <input
+          <Input
             type="text"
-            className="pl-3 bg-authInputBg text-white w-[199px] h-[62px] border-2 border-authInputBorder rounded-[14px] placeholder:text-authPlaceHolderColor text-[18px] font-bold"
+            className="placeholder:text-white text-white h-[50px]"
             placeholder="last name"
-            {...register('lastName', { required: true })}
+            {...register("lastName", { required: true })}
           />
         </div>
-        <input
+        <Input
           type="text"
-          className="pl-3 bg-authInputBg text-white h-[62px] border-2 border-authInputBorder rounded-[14px] placeholder:text-authPlaceHolderColor text-[18px] font-bold"
+          className="placeholder:text-white text-white h-[50px]"
           placeholder="username"
-          {...register('userName', { required: true })}
+          {...register("userName", { required: true })}
         />
-        <input
+        <Input
           type="email"
-          className="pl-3 bg-authInputBg text-white h-[62px] border-2 border-authInputBorder rounded-[14px] text-[18px] placeholder:text-authPlaceHolderColor font-bold"
+          className="placeholder:text-white text-white h-[50px]"
           placeholder="email"
-          {...register('email', { required: true })}
+          {...register("email", { required: true })}
         />
         <div className="relative">
-          <input
-            type={!showPassWord ? 'password' : 'text'}
-            className="pl-3 bg-authInputBg w-full h-[62px] text-white border-2 border-authInputBorder rounded-[14px] placeholder:text-authPlaceHolderColor font-bold text-[18px] pr-[50px]" // Add padding-right to make space for the icon
+          <Input
+            type={!showPassWord ? "password" : "text"}
+            className="placeholder:text-white text-white h-[50px]" // Add padding-right to make space for the icon
             placeholder="password"
-            {...register('passWord', {
+            {...register("passWord", {
               required: true,
               maxLength: 20,
               minLength: 4,
@@ -92,27 +95,27 @@ export const SignUpForm = ({ toggle }: { toggle: (arg: boolean) => void }) => {
           />
           {!showPassWord ? (
             <FaEye
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 w-[24px] h-[24px] cursor-pointer text-authPlaceHolderColor"
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 w-[24px] h-[24px] cursor-pointer text-white"
               onClick={ToggleShowPassWord}
             />
           ) : (
             <FaEyeSlash
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 w-[24px] h-[24px] cursor-pointer text-authPlaceHolderColor"
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 w-[24px] h-[24px] cursor-pointer text-white"
               onClick={ToggleShowPassWord}
             />
           )}
         </div>
-        <button
+        <Button
           type="submit"
-          className="w-[224px] h-[52px] rounded-[14px] bg-main mx-auto"
+          className="w-full h-[50px] rounded-[14px] bg-main hover:bg-main mx-auto hover:text-primary-foreground"
         >
-          <p className="text-[#AAADFA] font-bold text-[18px]">sign up</p>
-        </button>
+          <p className="text-[#AAADFA] font-semibold text-[14px] ">sign up</p>
+        </Button>
       </form>
-      <div className="h-[2px] w-[507px] mx-auto bg-[#A59999]"></div>
-      <div className="w-[420px] flex flex-col gap-7">
-        <button
-          className="h-[54px]  bg-white rounded-[10px]"
+      <Separator className=" bg-[#A59999] max-w-[80%] mt-5" />
+      <div className="flex flex-col gap-7 mt-5 w-[60%]">
+        <Button
+          className="h-[50px] bg-white rounded-[10px] hover:bg-white hover:text-black text-black"
           onClick={() =>
             forwardToLink(
               `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google/login`
@@ -121,13 +124,13 @@ export const SignUpForm = ({ toggle }: { toggle: (arg: boolean) => void }) => {
         >
           <div className="flex justify-center items-center gap-3">
             <FcGoogle className="h-[30px] w-[30px]" />
-            <p className="text-authPlaceHolderColor font-bold text-[18px]">
+            <p className="text-authPlaceHolderColor font-semibold text-[14px]">
               sign up with google
             </p>
           </div>
-        </button>
-        <button
-          className="h-[54px]  bg-[#201E1E] rounded-[10px]"
+        </Button>
+        <Button
+          className="h-[50px] bg-[#201E1E] rounded-[10px] hover:bg-[#201E1E]"
           onClick={() =>
             forwardToLink(
               `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/42/login`
@@ -136,9 +139,11 @@ export const SignUpForm = ({ toggle }: { toggle: (arg: boolean) => void }) => {
         >
           <div className="flex justify-center items-center gap-3">
             <Si42 className="h-[30px] w-[30px] text-white" />
-            <p className="text-white font-bold text-[18px]">sign up with 42</p>
+            <p className="text-white font-semibold text-[14px]">
+              sign up with 42
+            </p>
           </div>
-        </button>
+        </Button>
       </div>
     </>
   );
@@ -153,7 +158,7 @@ export const LoginForm = () => {
     try {
       window.location.href = link;
     } catch (error: unknown) {
-      console.log('error in google auth:', error);
+      console.log("error in google auth:", error);
     }
   };
   const {
@@ -172,7 +177,7 @@ export const LoginForm = () => {
     try {
       const getLoggedUser = async () => {
         const res = await api.post(
-          '/auth/login',
+          "/auth/login",
           {
             userName: data.userName,
             passWord: data.password,
@@ -184,17 +189,17 @@ export const LoginForm = () => {
         return res;
       };
       const ret = await getLoggedUser();
-      console.log('Data In here');
+      console.log("Data In here");
       setToken(ret.data.accessToken);
-      localStorage.setItem('userId', ret.data.id);
-      localStorage.setItem('userName', ret.data.userName);
+      localStorage.setItem("userId", ret.data.id);
+      localStorage.setItem("userName", ret.data.userName);
       dispatch(
         setUser({ id: ret.data.id, signIn: true, userName: data.userName })
       );
-      router.push(ret.data.redirectUrl)
+      router.push(ret.data.redirectUrl);
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
-      notify('error in signing in');
+      notify("error in signing in");
     }
   };
   return (
@@ -202,64 +207,63 @@ export const LoginForm = () => {
       <form
         action="submit"
         onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-7 mt-3"
+        className="flex flex-col gap-7 mt-3 w-[60%] "
       >
-        <input
+        <Input
           type="text"
-          className="pl-3 w-[400px] bg-authInputBg text-white h-[62px] border-2 border-authInputBorder rounded-[14px] text-[18px] placeholder:text-authPlaceHolderColor font-bold"
-          placeholder="userName"
-          {...register('userName', { required: true })}
+          className="placeholder:text-white text-white h-[50px] "
+          color="white"
+          placeholder="Username"
+          {...register("userName", { required: true })}
         />
         <div className="relative">
-          <input
-            type={!showPassWord ? 'password' : 'text'}
-            className="pl-3 bg-authInputBg w-full h-[62px] text-white border-2 border-authInputBorder rounded-[14px] placeholder:text-authPlaceHolderColor font-bold text-[18px] pr-[50px]" // Add padding-right to make space for the icon
+          <Input
+            type={!showPassWord ? "password" : "text"}
+            className="h-[50px] text-white placeholder:text-white" // Add padding-right to make space for the icon
             placeholder="password"
             required
-            {...register('password', { required: true })}
+            // color="black"
+            {...register("password", { required: true })}
           />
           {!showPassWord ? (
             <FaEye
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 w-[24px] h-[24px] cursor-pointer text-authPlaceHolderColor"
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 w-[24px] h-[24px] cursor-pointer text-authPlaceHolderColor text-white"
               onClick={ToggleShowPassWord}
             />
           ) : (
             <FaEyeSlash
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 w-[24px] h-[24px] cursor-pointer text-authPlaceHolderColor"
+              className="absolute right-3 top-1/2 transform text-white -translate-y-1/2 w-[24px] h-[24px] cursor-pointer text-authPlaceHolderColor"
               onClick={ToggleShowPassWord}
             />
           )}
         </div>
-        <p className="text-[#AAADFA] font-bold text-[18px] hover:cursor-pointer">
+        <div className="w-full">
+          <Button type="submit" className="w-full h-[50px] text-[#AAADFA] rounded-[14px] bg-main hover:bg-main mx-auto">
+            Login
+          </Button>
+        </div>
+        <p className="text-[#AAADFA] hover:text-[#787ab3] duration-300 transition font-semibold text-[14px] hover:cursor-pointer underline">
           forgot password ?
         </p>
-        <button
-          type="submit"
-          className="w-[224px] h-[52px] rounded-[14px] bg-main mx-auto"
-        >
-          <p className="text-[#AAADFA] font-bold text-[18px] hover:scale-105 duration-100">
-            login
-          </p>
-        </button>
       </form>
-      <div className="h-[2px] w-[507px] mx-auto bg-[#A59999] my-6"></div>
-      <div className="w-[420px] flex flex-col gap-7">
-        <button
-          className="h-[54px]  bg-white rounded-[10px]"
+      <Separator className=" bg-[#A59999] max-w-[80%]" />
+      <div className="w-[60%] flex flex-col gap-7 pt-8">
+        <Button
+          className="h-[50px] bg-white text-black hover:bg-white hover:text-black"
           onClick={() =>
             forwardToLink(
               `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google/login`
             )
           }
         >
-          <div className="flex justify-center items-center gap-3">
+          <div className="flex justify-center items-center gap-3 w-full h-full">
             <FcGoogle className="h-[30px] w-[30px]" />
-            <p className="text-authPlaceHolderColor font-bold text-[18px]">
+            <p className="text-authPlaceHolderColor font-semibold text-[14px]">
               login with google
             </p>
           </div>
-        </button>
-        <button className="h-[54px]  bg-[#201E1E] rounded-[10px]">
+        </Button>
+        <Button className="h-[50px]  bg-[#201E1E] rounded-[10px] hover:bg-bg-[#201E1E] hover:text-primary-foreground">
           <div
             className="flex justify-center items-center gap-3"
             onClick={() =>
@@ -269,9 +273,11 @@ export const LoginForm = () => {
             }
           >
             <Si42 className="h-[30px] w-[30px] text-white" />
-            <p className="text-white font-bold text-[18px]">login with 42</p>
+            <p className="text-white font-semibold text-[14px]">
+              login with 42
+            </p>
           </div>
-        </button>
+        </Button>
       </div>
     </>
   );
